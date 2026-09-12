34b82669f9a42c8935e8f855814389c063464986
pp8379951@gmail.com <pp8379951@gmail.com>
2026-09-12 23:39:59 +0530

    Add register controller

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,17 +1,12 @@
 import multer from "multer";
 
-
-const crypto = require('crypto')
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp")
+    cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
-  
-      cb(null, file.originalname)
-    
-  }
-})
+    cb(null, file.originalname);
+  },
+});
 
-export const upload = multer({ storage,})
+export const upload = multer({ storage });
