bd08ab7fe052928f411b9b16969ce9ddcad6de43
pp8379951@gmail.com <pp8379951@gmail.com>
2026-09-16 22:32:26 +0530

    Fix typos in subscription model and update getCurrentUser response format

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,6 +1,6 @@
 import mongoose , {Schema} from "mongoose";
 
-const subsciptionSchema = new Schema({
+const subscriptionSchema = new Schema({
     subscriber : {
         type : Schema.Types.ObjectId, //one who is
         ref : "User"
@@ -14,4 +14,4 @@ const subsciptionSchema = new Schema({
 },{timestamps : true})
 
 
-export const Subsciption = mongoose.model("Subscription" , subscriptionSchema)
+export const Subscription = mongoose.model("Subscription" , subscriptionSchema)
