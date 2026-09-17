1096dbd148483ad57cc9911f9ed680dc5864f185
pp8379951@gmail.com <pp8379951@gmail.com>
2026-09-17 23:48:33 +0530

    fix: correct upload field name for cover image in user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -61,7 +61,7 @@ router
 
 router
   .route("/cover-image")
-  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
 router
   .route("/c/:username")
